2865044a0863bf15c1a20f11a6d6c3348250a07f
nheimann <nheimann@cellular.de>
2018-09-22 15:27:37 +0200

    dev

--- qoptical/__init__.py
+++ qoptical/__init__.py
@@ -4,12 +4,15 @@ from .hamilton import ReducedSystem
 from .kernel_opencl import opmesolve_cl_expect
 from .fstools import persist_fs, load_fs
 
-
 __all__ = [
-    'time_gatter',
-    'opmeslve_cl_expect',
+    # general
     'ReducedSystem',
+
+    # util
+    'time_gatter',
     'persist_fs',
     'load_fs',
+
+    # short-hand solvers
     'opmesolve_cl_expect',
 ]
