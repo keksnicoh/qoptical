dceaa73dbc500cb0b4e1446dfbcbd26721b63733
nheimann <nheimann@cellular.de>
2018-10-25 12:52:31 +0200

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="qoptical",
     packages=find_packages(),
-    setup_requires=["cython", "pybind11", "mako"],
+    setup_requires=["Cython", "pybind11", "mako"],
     install_requires=["numpy", "scipy", "pytest", "qutip"],
     #install_requires=["numpy", "scipy", "mako", "pybind11", "pyopencl", "pytest", "cython", "qutip"],
 )
